54f791735a8435b5309fc34c56ba26e8d431e3be
Moon <moonks90@gmail.com>
2022-05-03 00:17:15 -0400

    initial commit

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -42,7 +43,7 @@ SECRET_KEY = 'django-insecure-&6n6uv)hq^%ca@ahyja(06-f^7-fib25pr6b9h*94unp(vox!#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -147,3 +148,5 @@ STATICFILES_DIRS = [
 ]
 
 IMPORT_EXPORT_USE_TRANSACTIONS = True
+
+django_heroku.settings(locals())
